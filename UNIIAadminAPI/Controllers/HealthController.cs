8eacd09dd31e44150df7136da98f0d4bdcfbd6a3
Ivan Lazarenko <ivan.lazarenko.2004@gmail.com>
2025-01-27 20:30:57 +0200

    [add] Added Health endpoint

---
+++ UNIIAadminAPI/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using UNIIAadminAPI.Enums;
+using UNIIAadminAPI.Serializers;
+using UNIIAadminAPI.Services;
+
+namespace UNIIAadminAPI.Controllers
+{
+    [Route("health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
